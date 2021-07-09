e79bcaef8f1951121379d03529c13e77de8a8675
Arran Oakes <arran.oakes@gmail.com>
2021-07-09 12:42:16 +0100

    cleaned up code

--- src/takeaway.js
+++ src/takeaway.js
@@ -3,7 +3,6 @@ class TakeAway {
   constructor() {
     this.menu = {};
     this.basket = {};
-    this.basket_counter = 0;
   };
 
   addDish(dish, price) {
@@ -32,18 +31,6 @@ class TakeAway {
     return basket.reduce((sum,key) => sum + hash[key], 0);
   };
 
-  // let sum = arr.reduce((a,b) => a+b, 0);
-  //       console.log(sum);
-  //       return sum;
-
-  // let sample = { a: 1 , b: 2 , c:3 };
-
-  // console.log(`sum:${sum(sample)}`);
-
 };
 
 let takeaway = new TakeAway();
-takeaway.addDish("chow mein", 5);
-takeaway.addDish("fried rice", 3.50);
-takeaway.addDish("prawn crackers", 1.99);
-takeaway.showMenu();
